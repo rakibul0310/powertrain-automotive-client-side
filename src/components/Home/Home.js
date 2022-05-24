2aaa4cc10d391d8a09426c99a1ff63ec214e3021
rakibul-cse-diu <rakibul2180@gmail.com>
2022-05-24 11:37:49 +0600

    add parts section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Parts from '../Parts/Parts';
 import TopBanner from './TopBanner/TopBanner';
 
 const Home = () => {
+    const [parts, setParts] = useState([])
+    useEffect(() => {
+        fetch("parts.json")
+            .then(res => res.json())
+            .then(data => setParts(data))
+    }, [])
     return (
-        <div>
+        <div className=''>
             <TopBanner />
+            <div>
+                <h1 className='text-3xl font-bold text-primary underline decoration-secondary text-center my-9'>Parts</h1>
+                <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mx-6 my-12 gap-4'>
+                    {
+                        parts.map(part => <Parts part={part}></Parts>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Parts/Parts.js
+++ src/components/Parts/Parts.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Parts = () => {
+const Parts = ({ part }) => {
+    const { name, description, img, minOrder, availableQuantity, price } = part;
     return (
-        <div>
-            <h1>this is parts page</h1>
+        <div className="card card-compact bg-base-100 shadow-xl w-fit">
+            <figure><img className='' src={img} width={350} height={200} alt="parts" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>{description}</p>
+                <p>Min order: {minOrder} units</p>
+                <p>Available quantity: {availableQuantity} units</p>
+                <p>Price: ${price} /unit</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Order Now</button>
+                </div>
+            </div>
         </div>
     );
 };
