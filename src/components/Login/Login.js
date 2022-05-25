29f4fe4c4a4fa1a6bb539cab452d9a9dfbd676b6
rakibul-cse-diu <rakibul2180@gmail.com>
2022-05-26 00:55:37 +0600

    implement firebase login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,80 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import Loading from '../Shared/Loading/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => {
-        console.log("in on submit", data.email);
-        console.log("in on submit", data.password);
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || "/";
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    useEffect(() => {
+        if (user || googleUser) {
+            const getAccesToken = async () => {
+                await fetch(`http://localhost:5000/login`, {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({ email: googleUser?.user.email || user?.user.email })
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        localStorage.setItem('accessToken', data.accessToken);
+                    })
+            }
+            getAccesToken();
+            console.log(user?.user.email || googleUser?.user.email);
+            // return navigate(from, { replace: true });
+        }
+    }, [user, googleUser, navigate, from])
+
+
+    const handleTryAgain = () => window.location.reload()
+
+    if (error || googleError) {
+        return (
+            <div className='min-h-screen text-red-500 flex flex-col justify-center items-center text-3xl text-center'>
+                <p>Error: {error?.message || googleError?.message}</p>
+                <button onClick={handleTryAgain} className='btn px-6 py-2 btn-primaty text-xl font-medium mt-5'>Try again</button>
+            </div>
+        );
+    }
+    if (loading || googleLoading) {
+        return <Loading />;
+    }
+
+    const onSubmit = async data => {
+        await signInWithEmailAndPassword(data.email, data.password);
+        reset()
     };
+
+    const handleGoogleLogin = async () => {
+        await signInWithGoogle();
+    }
 
     return (
         <div className='flex flex-col justify-center items-center min-h-screen'>
             <h2 className='text-3xl font-bold text-primary underline decoration-secondary mb-5'>Login</h2>
-            <form className='form-control justify-center items-center text-left w-full' onSubmit={handleSubmit(onSubmit)}>
-                <input className='input input-bordered input-success w-full max-w-xs my-3' type="email" id='email' name='email' placeholder='test@email.com' {...register("email")} />
-                <input className='input input-bordered input-success w-full max-w-xs' type="password" id='password' name='password' placeholder='password' {...register("password", { required: true })} />
+            <form className='form-control justify-center items-center text-left w-full' onSubmit={handleSubmit(onSubmit)} >
+                <input className='input input-bordered input-success w-full max-w-xs my-3' type="email" id='email' name='email' placeholder='test@email.com' required {...register("email")} autoComplete="off" />
+                <input className='input input-bordered input-success w-full max-w-xs' type="password" id='password' name='password' placeholder='password' {...register("password", { required: true })} autoComplete="off" />
                 {errors.exampleRequired && <span>This field is required</span>}
                 <input className='btn btn-secondary mt-3 w-full max-w-xs text-base-100' type="submit" value="Login" />
                 <Link to="/forgotpassword"><span className='text-sm mt-1 text-blue-500 underline decoration-blue-500'>Forgot password?</span></Link>
@@ -30,7 +87,7 @@ const Login = () => {
                     <div className='w-[80px] h-[3px] bg-slate-200'></div>
                 </div>
                 <div>
-                    <button className='my-5 px-6 py-1 btn text-white border-0 bg-red-600 hover:bg-red-500'>
+                    <button onClick={handleGoogleLogin} className='my-5 px-6 py-1 btn text-white border-0 bg-red-600 hover:bg-red-500'>
                         <FontAwesomeIcon className='text-red-500 text-lg mx-3 bg-slate-100 p-2 rounded-full' icon={faGoogle} />
                         Login with Google
                     </button>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,22 +1,53 @@
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading/Loading';
 
 const Register = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => {
-        console.log("in on submit", data.email);
-        console.log("in on submit", data.password);
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+    const handleTryAgain = () => window.location.reload()
+
+    if (error || updating) {
+        return (
+            <div className='min-h-screen text-red-500 flex flex-col justify-center items-center text-3xl text-center'>
+                <p>Error: {error?.message || updateError?.message}</p>
+                <button onClick={handleTryAgain} className='btn px-6 py-2 bg-blue-500/70 hover:bg-blue-500 text-gray-700 text-xl font-medium mt-5'>Try again</button>
+            </div>
+        );
+    }
+    if (loading || updating) {
+        return <Loading />;
+    }
+
+    const onSubmit = async data => {
+        // console.log("in on submit", data.email);
+        // console.log("in on submit", data.password);
+        if (data.password === data.confrimPassword) {
+            await createUserWithEmailAndPassword(data.email, data.password);
+            await updateProfile({ displayName: data.name, photoURL: "https://i.ibb.co/b64Rx7t/5907.jpg" });
+            console.log("update done")
+        }
+        reset();
     };
     return (
         <div>
             <div className='flex flex-col justify-center items-center min-h-screen'>
                 <h2 className='text-3xl font-bold text-primary underline decoration-secondary mb-5'>Register</h2>
                 <form className='form-control justify-center items-center text-left w-full' onSubmit={handleSubmit(onSubmit)}>
-                    <input className='input input-bordered input-primary w-full max-w-xs my-3' type="text" id='name' name='name' placeholder='Mr. Jhon' {...register("name")} />
-                    <input className='input input-bordered input-primary w-full max-w-xs mb-3' type="email" id='email' name='email' placeholder='test@email.com' {...register("email")} />
+                    <input className='input input-bordered input-primary w-full max-w-xs my-3' type="text" id='name' name='name' placeholder='Mr. Jhon' {...register("name")} required />
+                    <input className='input input-bordered input-primary w-full max-w-xs mb-3' type="email" id='email' name='email' placeholder='test@email.com' {...register("email")} required />
                     <input className='input input-bordered input-primary w-full max-w-xs mb-3' type="password" id='password' name='password' placeholder='password' {...register("password", { required: true })} />
                     {errors.password && <span className='font-thin text-red-500'>This field is required</span>}
                     <input className='input input-bordered input-primary w-full max-w-xs' type="password" id='confrim-password' name='confrim-password' placeholder='Confrim password' {...register("confrimPassword", { required: true })} />
