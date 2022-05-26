87c3f9702250c737bb2e491e525ba40c7f505c92
rakibul-cse-diu <rakibul2180@gmail.com>
2022-05-26 18:05:28 +0600

    emplement purchase and update remaining quantity

--- src/components/Purchase/Purchase.js
+++ src/components/Purchase/Purchase.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../firebase.init';
 import Loading from '../Shared/Loading/Loading';
 
@@ -9,7 +10,7 @@ const Purchase = () => {
     const [item, setItem] = useState({});
     const [user, loading] = useAuthState(auth);
     const [crossQuantity, setCrossQuantity] = useState(true);
-    const [orderQuantity, setOrderQuantity] = useState(0);
+    const [orderQuantity, setOrderQuantity] = useState(12);
     useEffect(() => {
         const url = `http://localhost:5000/purchase/${id}`;
         fetch(url)
@@ -24,24 +25,61 @@ const Purchase = () => {
     const handleQuantity = (e) => {
         const value = parseInt(e.target.value);
         setOrderQuantity(value);
-        if (value < parseInt(item.minOrder)) {
+        if (value < parseInt(item.minOrder) || value > parseInt(item.availableQuantity)) {
             setCrossQuantity(false);
-            console.log(crossQuantity)
+            toast.error(`You can order minimum ${item.minOrder} and maximum ${item.availableQuantity} units`)
         } else {
             setCrossQuantity(true);
-            console.log(crossQuantity)
         }
     }
 
     const handlePlaceOrder = () => {
+        const orderItem = {
+            email: user.email,
+            productName: item.name,
+            productId: item._id,
+            quantity: orderQuantity
+        }
 
+        fetch('http://localhost:5000/placeorder', {
+            method: 'POST',
+            headers: {
+                "content-type": 'application/json'
+            },
+            body: JSON.stringify(orderItem),
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+        toast.success("Order placed successfully")
+        console.log(orderItem);
+
+        const updatedItem = {
+            name: item.name,
+            img: item.img,
+            description: item.description,
+            features: item.features,
+            minOrder: item.minOrder,
+            availableQuantity: parseInt(item.availableQuantity) - orderQuantity,
+            price: item.price
+        }
+
+        const url = `http://localhost:5000/updateparts/${item._id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                "content-type": 'application/json'
+            },
+            body: JSON.stringify(updatedItem),
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
     }
 
     return (
         <div className='py-9 px-3 flex justify-center items-center'>
-            <div class="card lg:card-side bg-[#ecebe8] w-full md:w-3/4 h-[500px] shadow-xl">
+            <div className="card lg:card-side bg-[#ecebe8] w-full md:w-3/4 h-[500px] shadow-xl">
                 <figure><img src={item.img} alt="Album" /></figure>
-                <div class="card-body">
+                <div className="card-body">
                     <form className='form-control justify-start items-start text-left w-full' >
                         <label htmlFor="email" className='font-medium'>Email</label>
                         <input className='input input-bordered input-success w-full max-w-xs my-3' type="email" id='email' name='email' defaultValue={user.email} disabled />
@@ -51,12 +89,13 @@ const Purchase = () => {
                         <label htmlFor="quantity" className='mb-2 font-medium'>Quantity</label>
                         <input onChange={handleQuantity} className='input input-bordered input-success w-full max-w-xs' type="number" id='quantity' name='quantity' defaultValue={item.minOrder} />
                     </form>
-                    <div class="card-actions justify-end">
+                    <div className="card-actions justify-end">
                         {
-                            crossQuantity ? <button onClick={handlePlaceOrder} class="btn btn-primary">Place order</button> :
-                                <input class="btn btn-primary" type="submit" value="Place order" disabled />
+                            crossQuantity ? <button onClick={handlePlaceOrder} className="btn btn-primary">Place order</button> :
+                                <input className="btn btn-primary" type="submit" value="Place order" disabled />
                         }
                     </div>
+                    <ToastContainer />
                 </div>
             </div>
         </div>
