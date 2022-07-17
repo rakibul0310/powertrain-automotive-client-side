9036a20e82ca0745bfdf12c4e9063958994ed441
rakibul-cse-diu <rakibul2180@gmail.com>
2022-07-17 16:59:05 +0600

    fix rendering

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import InvestorRelations from '../InvestorRelations/InvestorRelations';
 import Notices from '../Notices/Notices';
-import Technologies from '../Technologies/Technologies';
+import Parts from '../Parts/Parts';
 import Business from './Business/Business';
 import Reviews from './Reviews/Reviews';
 import TopBanner from './TopBanner/TopBanner';
 
 const Home = () => {
+    const [parts, setParts] = useState([]);
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
+        fetch("https://sheltered-wave-82643.herokuapp.com/parts")
+            .then(res => res.json())
+            .then(data => setParts(data))
+
         fetch("https://sheltered-wave-82643.herokuapp.com/reviews")
             .then(res => res.json())
             .then(data => setReviews(data))
@@ -18,7 +23,14 @@ const Home = () => {
     return (
         <div className=''>
             <TopBanner />
-            <Technologies />
+            <div>
+                <h1 className='text-3xl font-bold text-primary underline decoration-secondary text-center my-9'>Parts</h1>
+                <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mx-6 my-12 gap-4'>
+                    {
+                        parts.map(part => <Parts key={part._id} part={part}></Parts>)
+                    }
+                </div>
+            </div>
             <Business />
             <div className='flex flex-col justify-center items-center mb-9'>
                 <h1 className='text-3xl font-bold text-primary underline decoration-secondary text-center my-9'>Reviews({reviews.length})</h1>
